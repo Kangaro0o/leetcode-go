129b3060e6de94bcd87ee3f2ab6e33bcb3a23212
kelvinwliu <kelvinwliu@tencent.com>
2022-07-01 13:51:14 +0800

    26. 删除有序数组中的重复项

--- array/other/26.remove_duplicates_from_sorted_array.go
+++ array/other/26.remove_duplicates_from_sorted_array.go
@@ -1,7 +1,7 @@
 package other
 
 // 26. 删除有序数组中的重复项
-func removeDuplicates(nums []int) int {
+func removeDuplicates1(nums []int) int {
 	n := len(nums)
 	elem := nums[0]
 	elemIdx := 0
@@ -19,4 +19,20 @@ func removeDuplicates(nums []int) int {
 		}
 	}
 	return n
+}
+
+// 使用快慢指针
+// 0 0 1 1 2
+func removeDuplicates(nums []int) int {
+	// 使用快慢指针，慢指针走在后面，快指针走在前面探路，找到一个不重复的元素就赋值给慢指针，并让慢指针前进一步
+	// 这样就保证了 nums[0...slow]都是无重复元素，当快指针遍历完整个数组后，程序结束
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[fast] != nums[slow] {
+			slow++
+			nums[slow] = nums[fast]
+		}
+		fast++
+	}
+	return slow + 1
 }
